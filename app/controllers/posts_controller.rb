9adbc0857f975dec3a610459accd20f373b9c1f5
ck <thecurrencykid@gmail.com>
2016-04-10 18:58:25 -0700

    user_id migration added to posts table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  before_action :authenticate_user!, except: [:index,:show] 
   # GET /posts
   # GET /posts.json
   def index
@@ -24,7 +25,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
